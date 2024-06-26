dd89422d971b4ad93eade9a972e8465e7f93c573
Deepak Nema <nemadeep82@gmail.com>
2024-06-26 12:15:57 +0530

    Setup.py updated to load local packages

--- setup.py
+++ setup.py
@@ -8,3 +8,15 @@ setup(
     install_requires=["ctransformers","sentence-transformers","pinecone-client","langchain","flask","pypdf","python-dotenv"],
     packages=find_packages()
 )
+
+'''
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Boktiar Ahmed Bappy',
+    author_email= 'entbappy73@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
+'''
